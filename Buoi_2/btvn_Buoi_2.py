75051ef3c36b0396ca91fa86808cf502fa86bf5b
MinhHung13 <johny.minhhung@gmail.com>
2023-06-10 20:18:03 +0700

    Commit btvn_Buoi_2.py

---
+++ Buoi_2/btvn_Buoi_2.py
@@ -0,0 +1,12 @@
+ten = "Hưng"
+print("Tên của bạn là", end=" ")
+print(ten)
+tuoi = "13"
+print("Tuổi của bạn là", end=" ")
+print(tuoi)
+lop = "PTB01"
+print("Lớp của bạn là", end=" ")
+print(lop)
+truong = "MindX"
+print("Trường của bạn là", end=" ")
+print(truong)
